18b8a42ec4537518424ac5732889838d3100e2e0
Maksim Bocharnikov <mbocharnikov@alfabank.ru>
2023-01-15 02:19:44 +0400

    add hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import Header from './Header'
 
 const App: React.FC = () => {
-  const [counter, setCounter] = useState<number>(0);
+  const [counter, setCounter] = useState<number>(0)
 
   const handleIncreaseClick = () => {
-    setCounter((prev) => prev + 1);
-  };
+    setCounter((prev) => prev + 1)
+  }
 
   return (
     <>
+      <Header />
       <h4>Counter: {counter}</h4>
 
       <button onClick={handleIncreaseClick}>Increase</button>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
