17bc1f08ea9572f4d5940afa8716952bb0db5e2c
Maksim Bocharnikov <mbocharnikov@alfabank.ru>
2023-01-29 18:27:17 +0400

    add test react query

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,50 @@
-import React, { useState } from 'react'
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
+import React from 'react'
+
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+
 import Header from './Header'
 
+const Posts = [
+  { id: 1, title: 'Post 1' },
+  { id: 2, title: 'Post 2' }
+]
+
+async function wait (duration: number) {
+  return await new Promise((resolve) => setTimeout(resolve, duration))
+}
+
 const App: React.FC = () => {
-  const [counter, setCounter] = useState<number>(0)
+  const queryClient = useQueryClient()
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => await wait(1000).then(() => [...Posts])
+    // queryFn: () => Promise.reject('Fuck you')
+  })
 
-  const handleIncreaseClick = () => {
-    setCounter((prev) => prev + 1)
-  }
+  const { mutate: addPost, isLoading: addingPostLoading } = useMutation({
+    mutationFn: async (title: string) =>
+      await wait(1000).then(() =>
+        Posts.push({
+          id: new Date().getTime(),
+          title
+        })
+      ),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['posts'] })
+    }
+  })
 
+  if (isLoading || addingPostLoading) return <h1>Loading...</h1>
+  if (isError) return <h1>{JSON.stringify(error)}</h1>
   return (
     <>
       <Header />
-      <h4>Counter: {counter}</h4>
-
-      <button onClick={handleIncreaseClick}>Increase</button>
+      {data.map((p) => (
+        <div key={p.id}>{p.title}</div>
+      ))}
+      <button onClick={() => { addPost('new post') }}>Add New</button>
     </>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,22 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import App from './components/App'
+
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+)
+
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+      <ReactQueryDevtools />
+    </React.StrictMode>
+  </QueryClientProvider>
+)
